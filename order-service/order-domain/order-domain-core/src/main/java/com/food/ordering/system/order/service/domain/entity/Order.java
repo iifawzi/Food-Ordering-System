2142fa3321fcb31de658eb09e43f2e868434e93d
iifawzi <iifawzie@gmail.com>
2022-12-02 20:41:59 +0200

    Implementing the order domain service
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
@@ -106,7 +106,7 @@ public class Order extends AggregateRoot<OrderId> {
         }
     }
 
-    void initializeOrder() {
+    public void initializeOrder() {
         setId(new OrderId(UUID.randomUUID()));
         trackingId = new TrackingId(UUID.randomUUID());
         orderStatus = OrderStatus.PENDING;
