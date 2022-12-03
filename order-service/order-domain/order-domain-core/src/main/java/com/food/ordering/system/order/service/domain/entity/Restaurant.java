901ebb79f7a3773979a4e788ef349def76052558
iifawzi <iifawzie@gmail.com>
2022-12-03 18:33:25 +0200

    Implementing the Application service
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -31,7 +31,7 @@ public class Restaurant extends AggregateRoot<RestaurantId> {
         private Builder() {
         }
 
-        public static Builder newBuilder() {
+        public static Builder builder() {
             return new Builder();
         }
 
